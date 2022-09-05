2096d34bbed3502eb701545250f3d0d789359bfc
Saige <saige.leslie@playonsports.com>
2022-09-04 22:56:43 -0400

    Display a navigation bar as the app's Layout component

---
+++ community-stabilization-fund-api/src/components/Layout.tsx
@@ -0,0 +1,47 @@
+import { UserAdmin, Logout } from "@carbon/icons-react";
+import {
+  Header,
+  HeaderName,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
+  HeaderNavigation,
+  HeaderMenu,
+  HeaderMenuItem,
+  Theme
+} from "@carbon/react";
+import Link from "next/link";
+
+interface LayoutProps { children: JSX.Element };
+
+const Layout = ({children}: LayoutProps) => (
+  <div className="container">
+    <Theme theme="g100">
+      <Header aria-label="IBM Platform Name">
+        <HeaderName href="#" prefix="IBM">
+          [Platform]
+        </HeaderName>
+        <HeaderNavigation aria-label="IBM [Platform]">
+          <Link href="/form-responses"><HeaderMenuItem isCurrentPage={true} >Form Responses</HeaderMenuItem></Link>
+          <Link href="/checklists"><HeaderMenuItem >Checklists</HeaderMenuItem></Link>
+          <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
+          <HeaderMenu aria-label="Link 4" menuLinkName="Link 4">
+            <HeaderMenuItem href="#">Sub-link 1</HeaderMenuItem>
+            <HeaderMenuItem href="#">Sub-link 2</HeaderMenuItem>
+            <HeaderMenuItem href="#">Sub-link 3</HeaderMenuItem>
+          </HeaderMenu>
+        </HeaderNavigation>
+        <HeaderGlobalBar>
+          <HeaderGlobalAction aria-label="Admin Name" onClick={() => {}}>
+            <UserAdmin />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="Sign Out" onClick={() => {}}>
+            <Logout />
+          </HeaderGlobalAction>
+        </HeaderGlobalBar>
+      </Header>
+    </Theme>
+    {children}
+  </div>
+);
+
+export { Layout };
