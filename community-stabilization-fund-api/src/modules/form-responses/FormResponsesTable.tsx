af15e17d5138d20fb803687ff98d497e732a918e
Arnelia Leslie <arnelialeslie@gmail.com>
2022-09-17 21:44:29 -0400

    Add Table design and change/add new headers

--- community-stabilization-fund-api/src/modules/form-responses/FormResponsesTable.tsx
+++ community-stabilization-fund-api/src/modules/form-responses/FormResponsesTable.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import {
+  DataTable,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableBody,
+  TableCell,
+  DataTableRow,
+  Table,
+  DataTableHeader,
+  DataTableCustomRenderProps
+} from 'carbon-components-react';
 // import styles from './styles/FormResponsesTable.module.css';
 
 //TODO: Move to interface/type folder
 export interface FormResponse {
 	id: string;
+  submitted_on: string;
 	first_name: string;
 	last_name: string;
 	email: string;
@@ -11,11 +24,12 @@ export interface FormResponse {
 	phone_type: string;
 	address_id: string;
 	is_black: boolean;
-	is_local: boolean;
+	live_in_pittsburgh_atlanta: boolean;
+  live_in_southside_atlanta: boolean;
 	has_flu_symptoms: boolean;
 	household_members: number;
 	feminine_health_care_id: string;
-	item_requests: string;
+	packages_like_to_receive: string;
 	additional_information: string;
 	is_pick_up: boolean;
 	is_volunteering: boolean;
@@ -25,6 +39,7 @@ export interface FormResponse {
 
 const mockData: FormResponse = {
   id: "1234",
+  submitted_on: "03/04/2022 3:44 pm",
   first_name: "Malcolm",
   last_name: "Moses",
   email: "malc@aol.com",
@@ -32,11 +47,12 @@ const mockData: FormResponse = {
   phone_type: "Mobile",
   address_id: "2133",
   is_black: true,
-  is_local: true,
+  live_in_pittsburgh_atlanta: false,
+  live_in_southside_atlanta: true,
   has_flu_symptoms: false,
   household_members: 5,
   feminine_health_care_id: "yyyy",
-  item_requests: "all",
+  packages_like_to_receive: "all",
   additional_information: "N/A",
   is_pick_up: false,
   is_volunteering: true,
@@ -44,32 +60,58 @@ const mockData: FormResponse = {
   _is_interested_in_memberbership: false,
 };
 
-const formResponseQuestions = [ "Name", "Phone Number", "Address", "Distribution Method", "COVID concern"];
+const formResponseQuestions = [ "Submitted On","First Name", "Last Name", "Email", "Phone Number","Phone Type", "Address",
+  "Is Black", "Live In Pittsburgh Atlanta", "Live In Southside Atlanta", "Has Flu Symptoms","Household Members","Feminine Health Care",
+  "Packages Like To Receive", "Additional Information", "Is Pick Up", "Is Volunteering", "Is Subscribing", "Is Interested In Memberbership"];
 
-const createTableColumn = (formResponseQuestions: string[]) => {
+const createHeaders = (formResponseQuestions: string[]) => {
   return (
     formResponseQuestions.map(
-      (string, id) => <th key={'questions' + id}>{string}</th>
+      (header: string) => ({key: header.toLowerCase().replace(" ", "_"), header})
     )
   );
 };
 
-const createTableData = (formResponses: FormResponse) => {
-  return (
-    Object.values(formResponses).map(
-      (string, id) => <td key={'answers' + id}>{string}</td>
-    )
-  );
+const createRows = (formResponses: FormResponse[]) => {
+  return (formResponses)
 };
 
 // Form Responses As Prop Arguments
 // i.e. const ItemChecklistByRecipientAndBag = (props: FormResponse) => {
+//Have to make the boolean values show on the table
 const FormResponsesTable = () => {
+  const rows = createRows([mockData]) as DataTableRow<string>[];
+  const headers = createHeaders(formResponseQuestions) as DataTableHeader<string>[];
+  console.log({rows, headers});
   return (
-    <table style={{margin: "10em auto"}}>
-      <tr>{createTableColumn(formResponseQuestions)}</tr>
-      <tr>{createTableData(mockData)}</tr>
-    </table>
+    <div style={{margin: "10em auto"}}>
+      {/* <tr>{createTableColumn(formResponseQuestions)}</tr> */}
+      {/* wes<tr>{createTableData(mockData)}</tr> */}
+      <DataTable rows={rows} headers={headers}>
+        {({ rows, headers, getTableProps, getHeaderProps, getRowProps }:DataTableCustomRenderProps) => (
+          <Table {...getTableProps()}>
+            <TableHead>
+              <TableRow>
+                {headers.map((header) => (
+                  <TableHeader  {...getHeaderProps({ header })}>
+                    {header.header}
+                  </TableHeader>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow  {...getRowProps({ row })}>
+                  {row.cells.map((cell) => (
+                    <TableCell key={cell.id}>{cell.value}</TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
+      </DataTable>
+    </div>
   );
 };
 
