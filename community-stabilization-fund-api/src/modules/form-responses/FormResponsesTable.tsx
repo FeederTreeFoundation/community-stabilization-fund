6265ccbd0d7fb65b4952f056732ead05dd1e9da9
Saige <saige.leslie@playonsports.com>
2022-09-27 23:30:12 -0400

    (m) Clean up duplicate code

--- community-stabilization-fund-api/src/modules/form-responses/FormResponsesTable.tsx
+++ community-stabilization-fund-api/src/modules/form-responses/FormResponsesTable.tsx
@@ -1,60 +1,17 @@
 import React from "react";
 
 import { FORM_RESPONSE_QUESTIONS } from "./constants";
-import { DataTableHeader, DataTableRow } from "carbon-components-react/lib/components/DataTable";
 import { mapBooleanToResponse } from "./utils";
+import { formResponseMock } from "../../mocks";
+
+import type { DataTableHeader, DataTableRow } from "carbon-components-react/lib/components/DataTable";
+import type { FormResponse } from "../../db";
 
 import { BasicTable } from "../../components";
 
-import { formResponseMock } from "../../mocks";
 // import styles from './styles/FormResponsesTable.module.css';
 
-//TODO: Move to interface/type folder
-export interface FormResponse {
-	id: string;
-  submitted_on: string;
-	first_name: string;
-	last_name: string;
-	email: string;
-	phone_number: string;
-	phone_type: string;
-	address_id: string;
-	is_black: boolean;
-	live_in_pittsburgh_atlanta: boolean;
-  live_in_southside_atlanta: boolean;
-	has_flu_symptoms: boolean;
-	household_members: number;
-	feminine_health_care_id: string;
-	packages_like_to_receive: string;
-	additional_information: string;
-	is_pick_up: boolean;
-	is_volunteering: boolean;
-	is_subscribing: boolean;
-	is_interested_in_memberbership: boolean;
-};
-
-const mockData: FormResponse = {
-  id: "1234",
-  submitted_on: "03/04/2022 3:44 pm",
-  first_name: "Malcolm",
-  last_name: "Moses",
-  email: "malc@aol.com",
-  phone_number: "555-5555",
-  phone_type: "Mobile",
-  address_id: "2133",
-  is_black: true,
-  live_in_pittsburgh_atlanta: false,
-  live_in_southside_atlanta: true,
-  has_flu_symptoms: false,
-  household_members: 5,
-  feminine_health_care_id: "yyyy",
-  packages_like_to_receive: "all",
-  additional_information: "N/A",
-  is_pick_up: false,
-  is_volunteering: true,
-  is_subscribing: true,
-  is_interested_in_memberbership: false,
-};
+const mockData = formResponseMock;
 
 interface FormResponsesTableProps {
   formResponses?: FormResponse[];
--- community-stabilization-fund-api/src/modules/form-responses/utils.ts
+++ community-stabilization-fund-api/src/modules/form-responses/utils.ts
@@ -1,7 +1,5 @@
-import { FormResponse } from "./FormResponsesTable";
-
 export const mapBooleanToResponse = (response: any, question: string) => {
-  const key = question.toLowerCase().replaceAll(" ", "_") as keyof FormResponse;
+  const key = question.toLowerCase().replaceAll(" ", "_");
   if(typeof response[key] === "boolean") {
     response[key] = !!response[key] ? "Yes" : "No";
   }
