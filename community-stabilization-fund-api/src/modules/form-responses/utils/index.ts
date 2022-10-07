717fdfa87f9cc8d703dad53a68cae912fd8a28ac
Saige <saige.leslie@playonsports.com>
2022-10-06 20:49:40 -0400

    (m) Remove unnecessary double negative

--- community-stabilization-fund-api/src/modules/form-responses/utils/index.ts
+++ community-stabilization-fund-api/src/modules/form-responses/utils/index.ts
@@ -1,6 +1,6 @@
 export const mapBooleanToResponse = (response: any, question: string) => {
   const key = question.toLowerCase().replaceAll(" ", "_");
   if(typeof response[key] === "boolean") {
-    response[key] = !!response[key] ? "Yes" : "No";
+    response[key] = response[key] ? "Yes" : "No";
   }
 };
