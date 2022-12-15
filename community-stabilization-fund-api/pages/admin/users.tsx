ddf4974b0ef1452349c3693b7cbda6274cd4c782
Meta Blvk <metablvk@gmail.com>
2022-12-14 19:04:56 -0700

    Moved users to admin folder, and created a users style module with default styles

--- community-stabilization-fund-api/pages/admin/users.tsx
+++ community-stabilization-fund-api/pages/admin/users.tsx
@@ -3,16 +3,20 @@ import { User } from '../src/modules';
 import { GetServerSideProps } from 'next';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-
+import styles from '../../styles/User.module.css';
 const UserPage: NextPage = () => {
   const [users, setUsers] = useState<User[] | null>(null);
   useEffect(() => {
     axios
       .get<User[]>('http://localhost:3000/api/users')
-      .then(({data}) => setUsers(data));
+      .then(({ data }) => setUsers(data));
   }, []);
   const user = users ? users[0] : {};
-  return <h1>Hi,{`${user?.name || ''}`}.</h1>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.intro}>Hi,{`${user?.name || ''}`}.</h1>
+    </div>
+  );
 };
 
 export default UserPage;
