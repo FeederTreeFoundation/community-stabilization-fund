43ec8d3003c26eb5c8fa191fe3c2fd3e938b8040
Saige <saige.leslie@playonsports.com>
2023-01-06 00:38:27 -0500

    (m) Update formresponse fields

--- community-stabilization-fund-api/src/db/models/index.ts
+++ community-stabilization-fund-api/src/db/models/index.ts
@@ -6,13 +6,13 @@ export interface FormResponse {
 	email: string;
 	phone_number: string;
 	phone_type: string;
-	address_id: string;
+	address_id?: string;
 	is_black: boolean;
 	live_in_pittsburgh_atlanta: boolean;
     live_in_southside_atlanta: boolean;
 	has_flu_symptoms: boolean;
 	household_members: number;
-	feminine_health_care_id: string;
+	feminine_health_care_id? : string;
 	packages_like_to_receive: string;
 	additional_information: string;
 	is_pick_up: boolean;
