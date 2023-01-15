fed68c6aa282a352ec3cc079fcdd7518ece91bf0
metablvk <metablvk@gmail.com>
2023-01-14 17:05:25 -0700

    Added in public runtime config

--- community-stabilization-fund-api/src/services/form-response/index.tsx
+++ community-stabilization-fund-api/src/services/form-response/index.tsx
@@ -1,29 +1,29 @@
-import axios from 'axios';
-import getConfig from 'next/config';
-import React from 'react';
+import axios from "axios";
+import getConfig from "next/config";
+import React from "react";
 
-import type { FormResponse } from '../../db';
+import type { FormResponse } from "../../db";
 const { publicRuntimeConfig } = getConfig() || {};
 const baseUrl = `${publicRuntimeConfig}.apiUrl/form-response`;
 
 let instance = axios.create({
   headers: {
     common: {
-      authorization: '',
+      authorization: "",
     },
   },
 });
 
 const getAllFormResponses = async () => {
   const res = await instance.get<FormResponse[]>(
-    `http://localhost:3000/api/form-responses`
+    `${publicRuntimeConfig?.apiUrl}/form-responses`
   );
   return res;
 };
 
 const createFormResponse = async () => {
   const res = await instance.post<FormResponse>(
-    `http://localhost:3000/api/form-responses`
+    `${publicRuntimeConfig?.apiUrl}/form-responses`
   );
   return res;
 };
