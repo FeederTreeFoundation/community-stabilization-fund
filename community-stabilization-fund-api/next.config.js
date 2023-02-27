18ee83af766f1f8f40414f4eda353865f1194157
Saige <saige.leslie@playonsports.com>
2023-02-26 19:09:12 -0500

    Use reserved env variable

--- community-stabilization-fund-api/next.config.js
+++ community-stabilization-fund-api/next.config.js
@@ -4,12 +4,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   serverRuntimeConfig: {
-      secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
+    secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
   },
   publicRuntimeConfig: {
-      apiUrl: process.env.NODE_ENV === 'production'
-          ? 'http://localhost:3000/api' // development api
-          : 'http://localhost:3000/api' // production api
+    apiUrl: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api` // development api
+      : 'http://localhost:3000/api' // production api
   }
 };
 
