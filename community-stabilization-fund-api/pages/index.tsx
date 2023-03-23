23fe29024727c1aeea15e6e55575bfb37662cb11
metablvk <metablvk@gmail.com>
2023-03-23 09:17:37 -0600

    Switched to title variable

--- community-stabilization-fund-api/pages/index.tsx
+++ community-stabilization-fund-api/pages/index.tsx
@@ -5,19 +5,22 @@ import type { NextPage } from 'next';
 
 import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => (
-  <div>
-    <Head>
-      <title>Community Stabalization Fund API | Home</title>
-      <meta name='description' content='Community Stabilization Funds API' />
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
-    <div className={styles.bg}>
-      <div className={styles.bg_overlay}>
-        <h1>Welcome to Community Stabilization Fund&apos;s API!</h1>
+const Home: NextPage = () => {
+  const title = `Welcome to Community Stabilization Fund\'s API!`;
+  return (
+    <div>
+      <Head>
+        <title>Community Stabalization Fund API | Home</title>
+        <meta name='description' content='Community Stabilization Funds API' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className={styles.bg}>
+        <div className={styles.bg_overlay}>
+          <h1>{title}</h1>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
