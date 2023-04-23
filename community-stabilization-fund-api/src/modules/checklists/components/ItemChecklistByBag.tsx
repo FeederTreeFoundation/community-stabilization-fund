e5ed41aca2813c9ceed3a155b656d8964d5cac0c
metablvk <metablvk@gmail.com>
2023-04-23 09:16:41 -0600

    added recipient info into bag labels

--- community-stabilization-fund-api/src/modules/checklists/components/ItemChecklistByBag.tsx
+++ community-stabilization-fund-api/src/modules/checklists/components/ItemChecklistByBag.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import { formResponseMock } from '../../../mocks';
 
-import { mapFormResponseToBagItems } from '../utils';
+import {
+  mapFormResponseToBagItems,
+  mapFormResponseToRecipientInfo,
+} from '../utils';
 
 import { BagLabels } from './BagLabels';
 
 import type { FormResponse } from '../../../db';
 
 import styles from '../styles/checklists.module.css';
-
 
 // TODO: Convert this to a 4x2.5 set of labels printed horizontally
 export interface ItemChecklistByBagProps {
@@ -21,13 +23,15 @@ const ItemChecklistByBag = ({
 }: ItemChecklistByBagProps) => {
   let labelCount = 0;
   const bagItemsMap = mapFormResponseToBagItems(formResponse);
-  const packages_selected = typeof formResponse.packages_to_receive === 'string' 
-    ? formResponse.packages_to_receive.split(',')
-    : formResponse.packages_to_receive;
+  const recipientInfo = mapFormResponseToRecipientInfo(formResponse);
 
   return (
     <div id='item-checklist' className={styles.item_checklist_wrapper}>
-      <BagLabels bagItemsMap={bagItemsMap} labelCount={labelCount} packages={packages_selected} />
+      <BagLabels
+        recipientInfo={recipientInfo}
+        bagItemsMap={bagItemsMap}
+        labelCount={labelCount}
+      />
     </div>
   );
 };
