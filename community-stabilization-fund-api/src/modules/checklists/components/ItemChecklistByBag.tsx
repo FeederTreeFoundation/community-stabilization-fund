e643d3adb2d9c9cbf623b2a47d7351ba9cb7d5fd
metablvk <metablvk@gmail.com>
2023-04-22 20:39:10 -0600

    added recipient info

--- community-stabilization-fund-api/src/modules/checklists/components/ItemChecklistByBag.tsx
+++ community-stabilization-fund-api/src/modules/checklists/components/ItemChecklistByBag.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import { formResponseMock } from '../../../mocks';
 
-import { mapFormResponseToBagItems } from '../utils';
+import {
+  mapFormResponseToBagItems,
+  mapFormResponseToRecipientInfo,
+} from '../utils';
 
 import { BagLabels } from './BagLabels';
 
 import type { FormResponse } from '../../../db';
 
 import styles from '../styles/checklists.module.css';
-
 
 // TODO: Convert this to a 4x2.5 set of labels printed horizontally
 export interface ItemChecklistByBagProps {
@@ -21,10 +23,15 @@ const ItemChecklistByBag = ({
 }: ItemChecklistByBagProps) => {
   let labelCount = 0;
   const bagItemsMap = mapFormResponseToBagItems(formResponse);
+  const recipientInfo = mapFormResponseToRecipientInfo(formResponse);
 
   return (
     <div id='item-checklist' className={styles.item_checklist_wrapper}>
-      <BagLabels bagItemsMap={bagItemsMap} labelCount={labelCount} />
+      <BagLabels
+        recipientInfo={recipientInfo}
+        bagItemsMap={bagItemsMap}
+        labelCount={labelCount}
+      />
     </div>
   );
 };
