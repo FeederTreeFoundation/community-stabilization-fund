864ce8c0a2d4b80646d9cc0a5842cdbb38b169e9
RobertTTaylor9517 <52056324+RobertTTaylor9517@users.noreply.github.com>
2023-04-24 17:11:32 -0400

    Added prisma ORM

--- community-stabilization-fund-api/pages/api/form-responses/index.ts
+++ community-stabilization-fund-api/pages/api/form-responses/index.ts
@@ -3,6 +3,9 @@ import { executeQuery, queries } from '../../../src/db';
 import type { FormResponse} from '../../../src/db';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient, Prisma } from '@prisma/client';
+
+const prisma = new PrismaClient()
 
 const formResponseHandler = (req: NextApiRequest, res: NextApiResponse) => {
   const { method, body } = req;
@@ -24,63 +27,118 @@ const formResponseHandler = (req: NextApiRequest, res: NextApiResponse) => {
   }
 };
 
+// const getAllFormResponses = async (res: NextApiResponse) => {
+//   const sql =
+//     `SELECT fr.*,
+//       fh.id as fh_id,
+//       fh.feminine_members,
+//       fh.hygiene_items,
+//       fh.needs_plan_b
+//     FROM form_response AS fr
+//     LEFT JOIN feminine_health_care AS fh
+//     ON fr.feminine_health_care_id = fh.id`;
+//
+//   try {
+//     const form_responses: FormResponse[] = await executeQuery({ sql });
+//
+//     return res.json([...(form_responses ?? [])]);
+//   } catch (error) {
+//     return res.json({ error });
+//   }
+// };
+
 const getAllFormResponses = async (res: NextApiResponse) => {
-  const sql = 
-    `SELECT fr.*, 
-      fh.id as fh_id, 
-      fh.feminine_members,
-      fh.hygiene_items,
-      fh.needs_plan_b
-    FROM form_response AS fr
-    LEFT JOIN feminine_health_care AS fh 
-    ON fr.feminine_health_care_id = fh.id`;
-  
-  try {
-    const form_responses: FormResponse[] = await executeQuery({ sql });
-
-    return res.json([...(form_responses ?? [])]);
-  } catch (error) {
-    return res.json({ error });
-  }
-};
-
-const createFormResponse = async (body: string, res: NextApiResponse) => {
-  const formResponse = JSON.parse(body);
-  const hygiene_items = formResponse["hygiene_items"].join(',');
-  
-  const fem_responses = {
-    "feminine_members": formResponse["feminine_members"],
-    "hygiene_items": hygiene_items,
-    "needs_plan_b": formResponse["needs_plan_b"]
-  };
-
-  const fem_sql = queries.makeCreateSql('feminine_health_care', fem_responses);
 
   try{
-    const result = await executeQuery({sql: fem_sql});
-    const {feminine_members, hygiene_items, needs_plan_b, ...rest } = formResponse;
-    const packages_to_receive = rest["packages_to_receive"].join(',');
+    const form_responses: FormResponse[] = await prisma.form_response.findMany({
+      include: {
+        feminine_health_care: true,
+        address: true
+      }
+    });
 
-    rest["packages_to_receive"] = packages_to_receive;
-    rest["feminine_health_care_id"] = result.insertId;
-
-    const sql = queries.makeCreateSql('form_response', rest);
-
-    try {
-      const result = await executeQuery({ sql });
-      console.log({ result });
-
-      return res
-        .status(201)
-        .send('Successfully created form response with id: ' + result.insertId);
-    } catch (error) {
-      return res.json({ error });
-    }
+    return res.json([...(form_responses ?? [])]);
 
   } catch (error) {
     return res.json({error});
   }
-};
+}
+
+const createFormResponse = async (body: string, res: NextApiResponse) => {
+  const request = JSON.parse(body);
+  const hygieneItems = request['hygiene_items'].join();
+
+  const {feminine_members, needs_plan_b, hygiene_items, address_city, address_state, address_zip, address_country, address_line1, address_line2, ...rest} = request
+
+  const formResponse = {
+    ...rest,
+    feminine_health_care: {
+      create: {
+        feminine_members: feminine_members,
+        hygiene_items: hygieneItems,
+        plan_b: needs_plan_b
+      }
+    },
+    address: {
+      create: {
+        city: address_city,
+        state: address_state,
+        zipcode: address_zip,
+        line1: address_line1,
+        line2: address_line2
+      }
+    }
+  }
+
+  try {
+    const result = await prisma.form_response.create({data: formResponse})
+
+    return res
+        .status(201)
+        .send('Successfully created form response with id: ' + result.id);
+  } catch (error) {
+    return res.json({error});
+  }
+
+}
+
+// const createFormResponse = async (body: string, res: NextApiResponse) => {
+//   const formResponse = JSON.parse(body);
+//   const hygiene_items = formResponse["hygiene_items"].join(',');
+//
+//   const fem_responses = {
+//     "feminine_members": formResponse["feminine_members"],
+//     "hygiene_items": hygiene_items,
+//     "needs_plan_b": formResponse["needs_plan_b"]
+//   };
+//
+//   const fem_sql = queries.makeCreateSql('feminine_health_care', fem_responses);
+//
+//   try{
+//     const result = await executeQuery({sql: fem_sql});
+//     const {feminine_members, hygiene_items, needs_plan_b, ...rest } = formResponse;
+//     const packages_to_receive = rest["packages_to_receive"].join(',');
+//
+//     rest["packages_to_receive"] = packages_to_receive;
+//     rest["feminine_health_care_id"] = result.insertId;
+//
+//     const sql = queries.makeCreateSql('form_response', rest);
+//
+//     try {
+//       const result = await executeQuery({ sql });
+//       console.log({ result });
+//
+//       return res
+//         .status(201)
+//         .send('Successfully created form response with id: ' + result.insertId);
+//     } catch (error) {
+//       return res.json({ error });
+//     }
+//
+//   } catch (error) {
+//     return res.json({error});
+//   }
+// };
 
 const deleteAllFormResponses = async (res: NextApiResponse) => {
   const sql = queries.truncateTableSql('form_response');
--- community-stabilization-fund-api/src/db/models/index.ts
+++ community-stabilization-fund-api/src/db/models/index.ts
@@ -1,38 +1,3 @@
-export interface FormResponse {
-  id: string;
-  first_name: string;
-  last_name: string;
-  email: string;
-  phone_number: string;
-  phone_type: string;
-  address_id?: number;
-  is_black?: boolean;
-  is_local?: boolean;
-  household_members?: number;
-  has_flu_symptoms?: boolean;
-  packages_to_receive?: string | string[];
-  feminine_health_care_id?: number;
-  item_requests?: string;
-  additional_information?: string;
-  is_pick_up?: boolean;
-  is_volunteering?: boolean;
-  is_subscribing?: boolean;
-  is_joining?: boolean;
-  is_interested_in_membership?: boolean;
-  address_city?: string;
-  address_country?: string;
-  address_line1?: string;
-  address_line2?: string;
-  address_state?: string;
-  address_zip?: string;
-  submitted_on?: string;
-  live_in_pittsburgh_atlanta?: boolean;
-  live_in_southside_atlanta?: boolean;
-  feminine_members?: number;
-  hygiene_items?: string | string[];
-  needs_plan_b?: boolean;
-}
-
 export interface User {
   id: number;
   name: string;
@@ -41,6 +6,42 @@ export interface User {
 export interface FeminineHealthResponse {
   id: number;
   feminine_members: number;
-  hygiene_items: number[];
+  hygiene_items: string|null;
   plan_b?: boolean;
 }
+
+export interface Address {
+  id: number;
+  city: string;
+  state: string;
+  zipcode: number;
+  line1: string;
+  line2: string|null;
+}
+
+export interface FormResponse {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email?: string|null;
+  phone_number: string;
+  phone_type?: string|null;
+  address: Address|null;
+  is_black: boolean;
+  is_local: boolean;
+  household_members?: number|null;
+  has_flu_symptoms: boolean;
+  packages_to_receive: string | string[];
+  feminine_health_care: FeminineHealthResponse|null;
+  item_requests?: string|null;
+  additional_information?: string|null;
+  is_pick_up: boolean;
+  is_volunteering: boolean;
+  is_subscribing: boolean;
+  is_joining: boolean;
+  is_interested_in_membership: boolean;
+  submitted_on?: string|null;
+  live_in_pittsburgh_atlanta?: boolean;
+  live_in_southside_atlanta?: boolean;
+}
+
