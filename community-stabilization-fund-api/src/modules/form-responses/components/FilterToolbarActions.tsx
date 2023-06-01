e0197ffd1261696c846ad4bd6f0a685358236367
metablvk <metablvk@gmail.com>
2023-05-31 19:55:09 -0600

    chore: added in onFilter fn for reusability

--- community-stabilization-fund-api/src/modules/form-responses/components/FilterToolbarActions.tsx
+++ community-stabilization-fund-api/src/modules/form-responses/components/FilterToolbarActions.tsx
@@ -12,49 +12,49 @@ interface FilterToolbarActionProps {
 const FilterToolbarActions = ({
   filterState,
   handleFilter,
-}: FilterToolbarActionProps) => (
-  <div className={`${styles.table_toolbar_action}`}>
-    <Checkbox
-      labelText={`Is Black?`}
-      id='checkbox-label-1'
-      value='is_black'
-      checked={filterState.includes('is_black')}
-      onChange={(e: ChangeEvent) =>
-        handleFilter((e.target as HTMLInputElement).value)
-      }
-      className={`${styles.filter_checkbox}`}
-    />
-    <Checkbox
-      labelText={`Is Local?`}
-      id='checkbox-label-2'
-      value='is_local'
-      checked={filterState.includes('is_local')}
-      onChange={(e: ChangeEvent) =>
-        handleFilter((e.target as HTMLInputElement).value)
-      }
-      className={`${styles.filter_checkbox}`}
-    />
-    <Checkbox
-      labelText={`Lives in south side Atlanta?`}
-      id='checkbox-label-3'
-      value='live_in_southside_atlanta'
-      checked={filterState.includes('live_in_southside_atlanta')}
-      onChange={(e: ChangeEvent) =>
-        handleFilter((e.target as HTMLInputElement).value)
-      }
-      className={`${styles.filter_checkbox}`}
-    />
-    <Checkbox
-      labelText={`Lives in Pittsburgh Atlanta?`}
-      id='checkbox-label-4'
-      value='live_in_pittsburgh_atlanta'
-      checked={filterState.includes('live_in_pittsburgh_atlanta')}
-      onChange={(e: ChangeEvent) =>
-        handleFilter((e.target as HTMLInputElement).value)
-      }
-      className={`${styles.filter_checkbox}`}
-    />
-  </div>
-);
+}: FilterToolbarActionProps) => {
+  function onFilter(e: ChangeEvent) {
+    if (!handleFilter) return;
+
+    const { value } = e.target as HTMLInputElement;
+    handleFilter(value);
+  }
+  return (
+    <div className={`${styles.table_toolbar_action}`}>
+      <Checkbox
+        labelText={`Is Black?`}
+        id='checkbox-label-1'
+        value='is_black'
+        checked={filterState.includes('is_black')}
+        onChange={onFilter}
+        className={`${styles.filter_checkbox}`}
+      />
+      <Checkbox
+        labelText={`Is Local?`}
+        id='checkbox-label-2'
+        value='is_local'
+        checked={filterState.includes('is_local')}
+        onChange={onFilter}
+        className={`${styles.filter_checkbox}`}
+      />
+      <Checkbox
+        labelText={`Lives in south side Atlanta?`}
+        id='checkbox-label-3'
+        value='live_in_southside_atlanta'
+        checked={filterState.includes('live_in_southside_atlanta')}
+        onChange={onFilter}
+        className={`${styles.filter_checkbox}`}
+      />
+      <Checkbox
+        labelText={`Lives in Pittsburgh Atlanta?`}
+        id='checkbox-label-4'
+        value='live_in_pittsburgh_atlanta'
+        checked={filterState.includes('live_in_pittsburgh_atlanta')}
+        onChange={onFilter}
+        className={`${styles.filter_checkbox}`}
+      />
+    </div>
+  );
+};
 
 export { FilterToolbarActions };
