1313b87a7fc3b597b4a5877225e7123eca5627a7
Meta Blvk <metablvk@gmail.com>
2023-06-11 12:44:10 -0600

    feat: created cmb page

---
+++ community-stabilization-fund-api/pages/about-us/community-movement-builders/index.tsx
@@ -0,0 +1,23 @@
+import BasicLayout from "../../../src/components/BasicLayout";
+import styles from "./community-movement-builders.module.css";
+
+const CommunityMovementBuilders = () => {
+  return (
+    <BasicLayout>
+      <h1>Community Movement Builders</h1>
+      <p>
+        COMMUNITY MOVEMENT BUILDERS is a 501(c)(3) member-based collective of
+        black people building sustainable, self-determining communities through
+        cooperative economic platforms and collective community organizing.
+      </p>
+      <div className={styles.contact}>
+        <h2>Contact Us</h2>
+        <a href='mailto:info@communitymovementbuilders.org'>
+          info@communitymovementbuilders.org
+        </a>
+      </div>
+    </BasicLayout>
+  );
+};
+
+export default CommunityMovementBuilders;
