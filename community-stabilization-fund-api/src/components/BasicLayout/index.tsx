184dbe34f66f0aa9f4dc19fee9c5f04ad398a06c
Meta Blvk <metablvk@gmail.com>
2023-06-11 12:45:40 -0600

    bugfix: ran linter

--- community-stabilization-fund-api/src/components/BasicLayout/index.tsx
+++ community-stabilization-fund-api/src/components/BasicLayout/index.tsx
@@ -1,18 +1,19 @@
-import { ReactNode } from "react";
 import BasicNavigation from "../BasicNavigation";
+
+import type { ReactNode } from "react";
+
+
 import styles from "./BasicLayout.module.css";
 
 interface BasicLayoutProps {
   children: ReactNode;
 }
 
-const BasicLayout = ({ children }: BasicLayoutProps) => {
-  return (
-    <div>
-      <BasicNavigation />
-      <main className={styles.container}>{children}</main>
-    </div>
-  );
-};
+const BasicLayout = ({ children }: BasicLayoutProps) => (
+  <div>
+    <BasicNavigation />
+    <main className={styles.container}>{children}</main>
+  </div>
+);
 
 export default BasicLayout;
