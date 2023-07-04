39f2e4ad1aea75a8a9b719861a04518540786950
Meta Blvk <metablvk@gmail.com>
2023-07-04 17:11:59 -0600

    feat: created dymo bag labels

--- community-stabilization-fund-api/src/modules/checklists/components/BagLabels.tsx
+++ community-stabilization-fund-api/src/modules/checklists/components/BagLabels.tsx
@@ -1,7 +1,7 @@
 import { RECIPIENT_INFORMATION_FIELDS } from '../constants';
 import { createBagItems } from '../utils';
 
-import BagList from './BagList';
+import { BagList, DymoBagOpOneList } from './BagList';
 
 import type { BagItemsMap } from '../types';
 
@@ -11,15 +11,108 @@ interface BagLabelsProps {
   //TODO: Why would this have instances of undefined?
   recipientInfo: (string | number | undefined)[];
   bagItemsMap: BagItemsMap;
-  packages?: string[]
+  packages?: string[];
   labelCount: number;
 }
+
+const DymoBagLabelsOpOne = ({
+  recipientInfo,
+  bagItemsMap,
+  labelCount,
+  packages,
+}: BagLabelsProps) => {
+  const groceryTHead = 'Groceries Bag';
+  const groceryItems = createBagItems('Groceries', bagItemsMap);
+
+  const getRecipientInfo = (text: string, id: number) => (
+    <>
+      <strong>{text}:</strong> {recipientInfo[id]}
+    </>
+  );
+
+  const recipientInfoList = RECIPIENT_INFORMATION_FIELDS.slice(0, 4).map(
+    (field, id) => (
+      <p className={styles.dymo_recipient_info} key={field + id}>
+        {getRecipientInfo(field, id)}
+      </p>
+    )
+  );
+  const grocerySlicePos = [
+    [0, 1],
+    [1, 4],
+    [4, 6],
+    [6, groceryItems.length],
+  ];
+
+  const groceryItemLabels =
+    packages?.includes('Food') &&
+    DymoBagOpOneList(
+      grocerySlicePos,
+      recipientInfoList,
+      groceryTHead,
+      groceryItems,
+      labelCount
+    );
+
+  const generalHygieneTHead = 'General Hygiene Bag';
+  const generalHygieneItems = createBagItems('General Hygiene', bagItemsMap);
+  const generalHygieneSlicePos = [[0, generalHygieneItems.length]];
+  const generalHygieneLabels =
+    packages?.includes('General Hygiene') &&
+    DymoBagOpOneList(
+      generalHygieneSlicePos,
+      recipientInfoList,
+      generalHygieneTHead,
+      generalHygieneItems,
+      labelCount
+    );
+
+  const cleaningHealthSupplyTHead = 'Cleaning/Health Supplies Bag';
+  const cleaningHealthSupplyItems = createBagItems(
+    'Cleaning/Health Supplies',
+    bagItemsMap
+  );
+  const cleaningHealthSlicePos = [
+    [0, 4],
+    [4, cleaningHealthSupplyItems.length],
+  ];
+  const cleaningHealthSupplyLabels =
+    packages?.includes('Cleaning/Health Supplies') &&
+    DymoBagOpOneList(
+      cleaningHealthSlicePos,
+      recipientInfoList,
+      cleaningHealthSupplyTHead,
+      cleaningHealthSupplyItems,
+      labelCount
+    );
+
+  const feminineHygieneTHead = 'Feminine Hygiene Bag';
+  const feminineHygieneItems = createBagItems('Feminine Hygiene', bagItemsMap);
+  const feminineHygieneSlicePos = [[0, feminineHygieneItems.length]];
+  const femineHygieneLabels =
+    packages?.includes('Feminine Health Care') &&
+    DymoBagOpOneList(
+      feminineHygieneSlicePos,
+      recipientInfoList,
+      feminineHygieneTHead,
+      feminineHygieneItems,
+      labelCount
+    );
+  return (
+    <div>
+      <div>{groceryItemLabels}</div>
+      <div>{generalHygieneLabels}</div>
+      <div>{cleaningHealthSupplyLabels}</div>
+      <div>{femineHygieneLabels}</div>
+    </div>
+  );
+};
 
 const BagLabels = ({
   recipientInfo,
   bagItemsMap,
   labelCount,
-  packages
+  packages,
 }: BagLabelsProps) => {
   const groceryTHead = 'Groceries Bag';
   const groceryItems = createBagItems('Groceries', bagItemsMap);
@@ -44,25 +137,29 @@ const BagLabels = ({
     [6, groceryItems.length],
   ];
 
-  const groceryItemLabels = packages?.includes('Food') && BagList(
-    grocerySlicePos,
-    recipientInfoList,
-    groceryTHead,
-    groceryItems,
-    labelCount
-  );
+  const groceryItemLabels =
+    packages?.includes('Food') &&
+    BagList(
+      grocerySlicePos,
+      recipientInfoList,
+      groceryTHead,
+      groceryItems,
+      labelCount
+    );
 
   labelCount = 0;
   const generalHygieneTHead = 'General Hygiene Bag';
   const generalHygieneItems = createBagItems('General Hygiene', bagItemsMap);
   const generalHygieneSlicePos = [[0, generalHygieneItems.length]];
-  const generalHygieneLabels = packages?.includes('General Hygiene') && BagList(
-    generalHygieneSlicePos,
-    recipientInfoList,
-    generalHygieneTHead,
-    generalHygieneItems,
-    labelCount
-  );
+  const generalHygieneLabels =
+    packages?.includes('General Hygiene') &&
+    BagList(
+      generalHygieneSlicePos,
+      recipientInfoList,
+      generalHygieneTHead,
+      generalHygieneItems,
+      labelCount
+    );
 
   labelCount = 0;
   const cleaningHealthSupplyTHead = 'Cleaning/Health Supplies Bag';
@@ -74,25 +171,29 @@ const BagLabels = ({
     [0, 4],
     [4, cleaningHealthSupplyItems.length],
   ];
-  const cleaningHealthSupplyLabels = packages?.includes('Cleaning/Health Supplies') && BagList(
-    cleaningHealthSlicePos,
-    recipientInfoList,
-    cleaningHealthSupplyTHead,
-    cleaningHealthSupplyItems,
-    labelCount
-  );
+  const cleaningHealthSupplyLabels =
+    packages?.includes('Cleaning/Health Supplies') &&
+    BagList(
+      cleaningHealthSlicePos,
+      recipientInfoList,
+      cleaningHealthSupplyTHead,
+      cleaningHealthSupplyItems,
+      labelCount
+    );
 
   labelCount = 0;
   const feminineHygieneTHead = 'Feminine Hygiene Bag';
   const feminineHygieneItems = createBagItems('Feminine Hygiene', bagItemsMap);
   const feminineHygieneSlicePos = [[0, feminineHygieneItems.length]];
-  const femineHygieneLabels = packages?.includes('Feminine Health Care') && BagList(
-    feminineHygieneSlicePos,
-    recipientInfoList,
-    feminineHygieneTHead,
-    feminineHygieneItems,
-    labelCount
-  );
+  const femineHygieneLabels =
+    packages?.includes('Feminine Health Care') &&
+    BagList(
+      feminineHygieneSlicePos,
+      recipientInfoList,
+      feminineHygieneTHead,
+      feminineHygieneItems,
+      labelCount
+    );
 
   return (
     <div className={styles.bag_labels_wrapper}>
@@ -106,4 +207,4 @@ const BagLabels = ({
   );
 };
 
-export { BagLabels };
+export { BagLabels, DymoBagLabelsOpOne };
