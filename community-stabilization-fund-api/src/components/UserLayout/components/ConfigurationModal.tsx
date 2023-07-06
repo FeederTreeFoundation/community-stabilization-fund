88ad2e1b8c458c04a29a27053c60cd96a0f442f1
FeederTreeLabs <saigeleslie@gmail.com>
2023-07-05 20:15:09 -0400

    Add delayedBy options to the ConfigurationModal

--- community-stabilization-fund-api/src/components/UserLayout/components/ConfigurationModal.tsx
+++ community-stabilization-fund-api/src/components/UserLayout/components/ConfigurationModal.tsx
@@ -1,6 +1,6 @@
-import { Modal } from "carbon-components-react";
+import { Checkbox, Modal } from "carbon-components-react";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 import type { ChecklistRule } from "../../../modules";
@@ -25,6 +25,7 @@ const ConfigurationModal = ({
   onRequestSubmit,
   onPackageChange,
 }: ConfigurationModalProps) => {
+  const [isDelayed, setIsDelayed] = useState(false);
   const {
     watch,
     register,
@@ -51,7 +52,7 @@ const ConfigurationModal = ({
       onRequestSubmit={handleSubmit(onRequestSubmit)}
     >
       <h5>Configure Package Rules</h5>
-      <p>
+      <p className="mt-2">
         In the package group
         <BasicSelect 
           id='package-dropdown'
@@ -60,6 +61,7 @@ const ConfigurationModal = ({
           defaultText='Choose a package'
           {...register('packageGroup', { required: true })}
           invalid={!!errors.packageGroup}
+          invalidText={errors.packageGroup?.message}
         />
         there should be 
         <BasicSelect
@@ -69,6 +71,7 @@ const ConfigurationModal = ({
           defaultText='Choose an item quantity'
           {...register('itemQuantity', { required: true })}
           invalid={!!errors.itemQuantity}
+          invalidText={errors.itemQuantity?.message}
         />
         per
         <BasicSelect 
@@ -78,17 +81,52 @@ const ConfigurationModal = ({
           defaultText='Choose an item'
           {...register('packageItem', { required: true })}
           invalid={!!errors.packageItem}
+          invalidText={errors.packageItem?.message}
         />
         for
         <BasicSelect
           id='household-members-dropdown'
-          items={["1","2","3","4","5","6","7","8","9","10"]}
+          items={["0", "1","2","3","4","5","6","7","8","9","10"]}
           noLabel
           defaultText='Choose # of household members'
           {...register('householdMembers', { required: true })}
           invalid={!!errors.householdMembers}
+          invalidText={errors.householdMembers?.message}
         />
         household members.
+      </p>
+      <p className="mt-4">
+        <Checkbox 
+            labelText={'Delay package item ' + (isDelayed ? ' by ' : '') }
+            id='is-delayed-checkbox'
+            onChange={() => setIsDelayed(!isDelayed)}
+        />
+        { isDelayed && (
+          <>
+            <BasicSelect
+              id='delay-dropdown'
+              items={["0","1","2","3","4","5","6","7","8","9","10"]}
+              noLabel
+              defaultText='Choose # of days'
+              defaultValue={0}
+              {...register('delayedBy.days')}
+              invalid={!!errors.delayedBy?.days}
+              invalidText={errors.delayedBy?.days?.message}
+            /> 
+             days and
+            <BasicSelect 
+              id='delay-dropdown'
+              items={["1","2","3","4","5","6","7","8","9","10"]}
+              noLabel
+              defaultValue={0}
+              defaultText='Choose # of weeks'
+              {...register('delayedBy.weeks')}
+              invalid={!!errors.delayedBy?.weeks}
+              invalidText={errors.delayedBy?.weeks?.message}
+            />
+            weeks.
+          </>
+        )}
       </p>
     </Modal>
   );
