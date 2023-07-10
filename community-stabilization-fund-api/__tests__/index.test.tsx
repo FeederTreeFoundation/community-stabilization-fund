764b2d263de3707592b8f74bd6192a80b40b4e42
FeederTreeLabs <saigeleslie@gmail.com>
2023-07-10 00:03:19 -0400

    Revert "mock dayjs"
    This reverts commit 38aa26bf96dbaec93f1a89c1e26c26486ed8ccca.

--- community-stabilization-fund-api/__tests__/index.test.tsx
+++ community-stabilization-fund-api/__tests__/index.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-
 import Home from '../pages/index';
 
 describe('Home', () => {
--- community-stabilization-fund-api/__tests__/snapshot.tsx
+++ community-stabilization-fund-api/__tests__/snapshot.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import MockDate from 'mockdate';
 import { render } from '@testing-library/react';
-
 import Home from '../pages/index';
 
-describe('Home', () => {
-  beforeAll(() => {
-    MockDate.set('2001-03-21')
-  });
-
-  afterAll(() => {
-    MockDate.reset()
-  });
-
-  it('renders homepage unchanged', () => {
-    const { container } = render(<Home />)
-    expect(container).toMatchSnapshot()
-  })
+it('renders homepage unchanged', () => {
+  const { container } = render(<Home />)
+  expect(container).toMatchSnapshot()
 })
