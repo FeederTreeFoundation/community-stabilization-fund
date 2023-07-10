f2f7c8f851ba96fbd4c887d50638ac80af3b4e15
Meta Blvk <metablvk@gmail.com>
2023-07-09 19:35:37 -0600

    feat: created dymo bag label

--- community-stabilization-fund-api/src/modules/checklists/components/BagLabel.tsx
+++ community-stabilization-fund-api/src/modules/checklists/components/BagLabel.tsx
@@ -1,5 +1,8 @@
-import { ItemChecklistTableColumn } from './ItemChecklistTableColumn';
-
+import {
+  DymoOpOneChecklistTableColumn,
+  ItemChecklistTableColumn,
+} from './ItemChecklistTableColumn';
+import Image from 'next/image';
 import styles from '../styles/checklists.module.css';
 
 interface BagLabelProps {
@@ -8,17 +11,31 @@ interface BagLabelProps {
   recipientInfoList: JSX.Element[];
 }
 
-const BagLabel = ({
-  thead,
-  items,
-  recipientInfoList,
-}: BagLabelProps) => (
+const BagLabel = ({ thead, items, recipientInfoList }: BagLabelProps) => (
   <div className={styles.bag_label}>
-    <div className={styles.user_bag_label_info}>
-      {recipientInfoList}
-    </div>
+    <div className={styles.user_bag_label_info}>{recipientInfoList}</div>
     <ItemChecklistTableColumn thead={thead} items={items ?? []} />
   </div>
 );
 
-export default BagLabel;
+const DymoBagOpOneLabel = ({
+  thead,
+  items,
+  recipientInfoList,
+}: BagLabelProps) => (
+  <div className={styles.dymo_op_one_bag_label}>
+    <div className={styles.dymo_op_one_bag_label_info}>{recipientInfoList}</div>
+    <DymoOpOneChecklistTableColumn thead={thead} items={items ?? []} />
+    <div className={styles.dymo_img_wrapper}>
+      <Image
+        src='/img/cmb_bag_label_logo.png'
+        alt='CMB Logo'
+        width={250}
+        height={250}
+        objectFit='contain'
+      />
+    </div>
+  </div>
+);
+
+export { BagLabel, DymoBagOpOneLabel };
