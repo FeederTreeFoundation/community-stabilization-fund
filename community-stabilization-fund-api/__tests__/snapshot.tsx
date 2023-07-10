38aa26bf96dbaec93f1a89c1e26c26486ed8ccca
FeederTreeLabs <saigeleslie@gmail.com>
2023-07-09 23:10:54 -0400

    mock dayjs

--- community-stabilization-fund-api/__tests__/snapshot.tsx
+++ community-stabilization-fund-api/__tests__/snapshot.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import MockDate from 'mockdate';
 import { render } from '@testing-library/react';
+
 import Home from '../pages/index';
 
-it('renders homepage unchanged', () => {
-  const { container } = render(<Home />)
-  expect(container).toMatchSnapshot()
+describe('Home', () => {
+  beforeAll(() => {
+    MockDate.set('2001-03-21')
+  });
+
+  afterAll(() => {
+    MockDate.reset()
+  });
+
+  it('renders homepage unchanged', () => {
+    const { container } = render(<Home />)
+    expect(container).toMatchSnapshot()
+  })
 })
