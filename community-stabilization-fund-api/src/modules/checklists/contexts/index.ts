5cd1d550984b6b586374848a321ab537c30a8d81
Meta Blvk <metablvk@gmail.com>
2023-07-09 20:47:51 -0600

    chore: added bag label type to context

--- community-stabilization-fund-api/src/modules/checklists/contexts/index.ts
+++ community-stabilization-fund-api/src/modules/checklists/contexts/index.ts
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext } from 'react';
 
 export interface ChecklistRule {
   householdMembers?: string;
@@ -9,11 +9,15 @@ export interface ChecklistRule {
     days?: number;
     weeks?: number;
   };
+  bagLabelType?: string;
 }
 
 interface ChecklistsRulesContextProps {
-  rules: ChecklistRule[], 
-  updateRules?: Function
+  rules: ChecklistRule[];
+  updateRules?: Function;
+  bagLabelType?: string;
+  updateBagLabelType?: Function;
 }
 
-export const ChecklistsRulesContext = createContext<ChecklistsRulesContextProps>({ rules: [] });
+export const ChecklistsRulesContext =
+  createContext<ChecklistsRulesContextProps>({ rules: [], bagLabelType: '' });
