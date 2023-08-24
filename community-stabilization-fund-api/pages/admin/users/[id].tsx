850f78ad621e5db7016fe5e10778aa9ba7097816
FeederTreeLabs <saigeleslie@gmail.com>
2023-08-23 22:12:03 -0400

    Change how the login redirect flow works

--- community-stabilization-fund-api/pages/admin/users/[id].tsx
+++ community-stabilization-fund-api/pages/admin/users/[id].tsx
@@ -1,12 +1,16 @@
 import { useUser } from '@auth0/nextjs-auth0';
 import { Button, TextInput, Modal } from 'carbon-components-react';
 import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useForm } from 'react-hook-form';
 
 import type { UserDTO } from '../../../src/db';
 
+import { useStorage } from '../../../src/hooks';
+import { ApiUserContext } from '../../../src/modules/users/contexts';
 import UserService from '../../../src/services/user';
+
+import { isEmpty } from '../../../src/utils';
 
 import styles from './users.module.css';
 
@@ -23,50 +27,47 @@ const AdminPage = () => {
   
   const router = useRouter();
   const { user, error, isLoading} = useUser();
+  const { state } = useStorage('api_user', '');
 
   const [isEditing, setIsEditing] = useState<boolean>(false);
-  const [apiUser, setApiUser] = useState<UserDTO>();
   const [apiError, setApiError] = useState<Error | undefined>(error);
+
+  const { apiUser, updateApiUser } = useContext(ApiUserContext);
   
-  const id = router.query.id;
-  const apiUserId = localStorage.getItem('api_user');
+  const {id, returnUrl } = router.query;
+  const apiUserId = `${state}`;
+
+  useEffect(() => {
+    setTimeout(() => {
+      if(!isEmpty(returnUrl) && !isEmpty(apiUser) && !isEmpty(apiUserId)) {
+        router.push(`${returnUrl}`);
+      }
+    }, 500);
+  }, [returnUrl, apiUser, apiUserId, router]);
 
   useEffect(() => {
     async function getApiUser(id?: string) {
-      if (!id || typeof id !== 'string') return;
+      if (isEmpty(id) || typeof id !== 'string') return;
 
       try {
         const results = await UserService.getById(id);
         
-        if (results?.data) setApiUser(results.data);
+        if (results?.data && updateApiUser) updateApiUser(results.data);        
       } catch (e) {
         setApiError(e as Error);
       }
     }
   
     function checkUserData() {
+      if (isEmpty(apiUserId) || typeof apiUserId !== 'string') return;
+      
       if(`${apiUserId}` === `${id}`) {
-        getApiUser(apiUserId ?? '');
+        getApiUser(apiUserId);
       }
     }
 
     checkUserData();
-  }, [apiUserId, id]);
-
-  const handleRevokeAdmin = () => {
-    UserService.logout();
-  };
-
-  const onSubmit = (data: FormData) => {
-    const updatedApiUser = { ...apiUser, name: data.username } as UserDTO;
-    UserService.update(`${apiUser?.id}`, updatedApiUser).then((_) => {
-      
-      setApiUser(updatedApiUser);
-    });
-    setIsEditing(false);
-  };
-
-  const handleEdit = () => setIsEditing(!isEditing);
+  }, [apiUserId, id, updateApiUser]);
 
   if(isLoading) return <>Loading...</>;
 
@@ -107,6 +108,25 @@ const AdminPage = () => {
       </Modal>
     </div>
   );
+
+  function onSubmit(data: FormData) {
+    if(typeof updateApiUser !== 'function') return;
+
+    const updatedApiUser = { ...apiUser, name: data.username } as UserDTO;
+    UserService.update(`${apiUser?.id}`, updatedApiUser).then((_) => {
+      
+      updateApiUser(updatedApiUser);
+    });
+    setIsEditing(false);
+  }
+
+  function handleEdit() {
+    setIsEditing(!isEditing);
+  }
+
+  function handleRevokeAdmin() {
+    UserService.logout();
+  }
 };
 
 export default AdminPage;
