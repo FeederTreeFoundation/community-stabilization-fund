0ebf445a94e6e5981e46f0501a975b628f09e9f6
FeederTreeLabs <saigeleslie@gmail.com>
2023-08-24 00:31:55 -0400

    Add new user path

---
+++ community-stabilization-fund-api/pages/forms/volunteers/groceries-and-supplies.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+import { GroceriesAndSuppliesForm } from '../../../src/modules/forms';
+
+const VolunteerGroceriesAndSupplies: NextPage = () => {
+  return <GroceriesAndSuppliesForm />;
+};
+
+export default VolunteerGroceriesAndSupplies;
