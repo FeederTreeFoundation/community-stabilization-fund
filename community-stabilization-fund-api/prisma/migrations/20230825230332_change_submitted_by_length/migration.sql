e593ce03f55679987f915864794771adf4e79d8b
FeederTreeLabs <saigeleslie@gmail.com>
2023-08-25 19:29:53 -0400

    Update schema for submitted_by

---
+++ community-stabilization-fund-api/prisma/migrations/20230825230332_change_submitted_by_length/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `submitted_on` on the `checklist_rule` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `submitted_on` on the `form_response` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+
+*/
+-- AlterTable
+ALTER TABLE `checklist_rule` MODIFY `submitted_on` DATETIME NOT NULL DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE `form_response` MODIFY `submitted_on` DATETIME NOT NULL DEFAULT NOW(),
+    MODIFY `submitted_by` VARCHAR(60) NULL;
