f36721b5da0ab043c6e5deefd4847bd6b703fa39
Meta Blvk <metablvk@gmail.com>
2023-09-11 16:19:05 -0600

    chore:lint fix

--- community-stabilization-fund-api/pages/api/form-responses/index.ts
+++ community-stabilization-fund-api/pages/api/form-responses/index.ts
@@ -91,14 +91,14 @@ const createFormResponse = async (body: any, res: NextApiResponse) => {
       'Feminine Health Care'
     )
       ? {
-          create: {
-            feminine_members: Number(feminine_health_care?.feminine_members),
-            hygiene_items: feminine_health_care?.hygiene_items?.join(),
-            needs_plan_b: Boolean(
-              feminine_health_care?.needs_plan_b === 'true'
-            ),
-          },
-        }
+        create: {
+          feminine_members: Number(feminine_health_care?.feminine_members),
+          hygiene_items: feminine_health_care?.hygiene_items?.join(),
+          needs_plan_b: Boolean(
+            feminine_health_care?.needs_plan_b === 'true'
+          ),
+        },
+      }
       : undefined,
     address: {
       create: {
