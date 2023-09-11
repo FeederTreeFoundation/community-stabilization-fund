7dff36fcf3c853592ed69c5c28829d8d3e8fec74
Meta Blvk <metablvk@gmail.com>
2023-09-11 16:13:26 -0600

    chore: removed console.log

--- community-stabilization-fund-api/pages/api/form-responses/index.ts
+++ community-stabilization-fund-api/pages/api/form-responses/index.ts
@@ -91,14 +91,14 @@ const createFormResponse = async (body: any, res: NextApiResponse) => {
       'Feminine Health Care'
     )
       ? {
-        create: {
-          feminine_members: Number(feminine_health_care?.feminine_members),
-          hygiene_items: feminine_health_care?.hygiene_items?.join(),
-          needs_plan_b: Boolean(
-            feminine_health_care?.needs_plan_b === 'true'
-          ),
-        },
-      }
+          create: {
+            feminine_members: Number(feminine_health_care?.feminine_members),
+            hygiene_items: feminine_health_care?.hygiene_items?.join(),
+            needs_plan_b: Boolean(
+              feminine_health_care?.needs_plan_b === 'true'
+            ),
+          },
+        }
       : undefined,
     address: {
       create: {
@@ -130,7 +130,7 @@ const updateBulkFormResponses = async (
   res: NextApiResponse
 ) => {
   const {feminine_health_care, address, ...rest} = body;
-  console.log({body});
+
   try {
     const result = await prisma.form_response.updateMany({
       where: {id: {in: ids.map((id) => parseInt(id))}},
