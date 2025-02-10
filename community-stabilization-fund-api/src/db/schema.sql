c44879ebfae1593837a6908b02aa7ae4c65b7bd3
FeederTreeFoundation <ogcsideways@aol.com>
2025-02-10 09:40:33 -0500

    feat: update seed script

--- community-stabilization-fund-api/src/db/schema.sql
+++ community-stabilization-fund-api/src/db/schema.sql
@@ -17,6 +17,8 @@ CREATE TABLE IF NOT EXISTS api_key (
   name VARCHAR(20),
   api_user_id INT, 
   FOREIGN KEY (api_user_id) REFERENCES api_user(id) ON DELETE CASCADE
+  organization_id INT,
+  FOREIGN KEY (organization_id) REFERENCES organization(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS organization (
@@ -33,6 +35,6 @@ INSERT INTO api_user (name)
 VALUES ("foo");
 
 INSERT INTO api_key
-(name, api_user_id)
+(name, api_user_id, organization_id)
 VALUES
-("bar", 1);
+("bar", 1, 1);
